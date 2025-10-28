1f9c756e0188f25d5d1a646924685a458ca112c6
陈博 <160844058+chenyuanjlu@users.noreply.github.com>
2025-10-28 18:09:19 +0800

    Update __init__.py

--- network/__init__.py
+++ network/__init__.py
@@ -4,10 +4,16 @@ Neural network models for knee ligament injury classification
 
 from .desnet import DenseNet169, DenseNet
 from .densenet_multiplane import MultiPlaneDenseNet, create_multiplane_densenet
+from .anklenet import AnkleNet
+from .mrnet import MRNet
+from .copas import CoPAS
 
 __all__ = [
     'DenseNet169',
     'DenseNet',
     'MultiPlaneDenseNet',
     'create_multiplane_densenet',
+    'AnkleNet',
+    'MRNet',
+    'CoPAS',
 ]
